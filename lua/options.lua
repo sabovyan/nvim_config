947fc5a85587b2f923ba682c24ca223a22fcb9ee
sabovyan <abovyan.sargis90@gmail.com>
2023-04-14 10:58:09 +0400

    add options

--- lua/options.lua
+++ lua/options.lua
@@ -1,40 +1,72 @@
--- [[ Setting options ]]
 -- See `:help vim.o`
 
--- Set highlight on search
-vim.o.hlsearch = false
+local opt = vim.opt
 
--- Make line numbers default
-vim.wo.number = true
+opt.hlsearch = false -- Set highlight on search
 
--- Enable mouse mode
-vim.o.mouse = 'a'
+opt.number = true         -- Make line numbers default
+opt.relativenumber = true -- Relative line numbers
+opt.mouse = 'a'           -- Enable mouse mode
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+
+opt.clipboard = "unnamedplus"
 
 -- Enable break indent
-vim.o.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.o.undofile = true
+opt.undofile = true
 
 -- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+opt.signcolumn = "yes"
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
+opt.timeout = true
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.timeoutlen = 500 -- speed must be under 500ms inorder for keys to work, increase if you are not able to.
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
+opt.completeopt = "menu,menuone,noselect"
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+opt.termguicolors = true
+
+opt.autowrite = true           -- Enable auto write
+opt.conceallevel = 3           -- Hide * markup for bold and italic
+opt.confirm = true             -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true          -- Enable highlighting of the current line
+opt.expandtab = true           -- Use spaces instead of tabs
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.inccommand = "nosplit" -- preview incremental substitute
+opt.incsearch = true
+opt.laststatus = 0
+opt.list = true                    -- Show some invisible characters (tabs...
+opt.pumblend = 10                  -- Popup blend
+opt.pumheight = 10                 -- Maximum number of entries in a popup
+opt.scrolloff = 5                  -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+opt.shiftround = true              -- Round indent
+opt.shiftwidth = 2                 -- Size of an indent
+opt.shortmess:append { W = true, I = true, c = true }
+opt.showmode = false               -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8              -- Columns of context
+opt.smartindent = true             -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.splitbelow = true              -- Put new windows below current
+opt.splitright = true              -- Put new windows right of current
+opt.tabstop = 2                    -- Number of spaces tabs count for
+opt.undolevels = 10000
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.winminwidth = 5                -- Minimum window width
+opt.wrap = false                   -- Disable line wrap
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
