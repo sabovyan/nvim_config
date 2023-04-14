bf08b1b9c56ea64fc48e40256edea9f4361e9cd7
sabovyan <abovyan.sargis90@gmail.com>
2023-04-14 11:58:02 +0400

    add rust and ts server

--- init.lua
+++ init.lua
@@ -287,9 +287,8 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-
+  rust_analyzer = {},
+  tsserver = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,29 @@
-return { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  }
-
-
+return {
+  -- Theme inspired by Atom
+  'navarasu/onedark.nvim',
+  priority = 1000,
+  config = function()
+    -- require('onedark').setup {
+    --   --   colors = {
+    --   --     bright_orange = "#ff8800", -- define a new color
+    --   --     green = '#00ffaa',         -- redefine an existing color
+    --   --   },
+    --   --
+    --   highlights = {
+    --     --     -- [] = {},
+    --     ["@variable"] = { fg = '#e5c07b' },
+    --     ["@parameter"] = { fg = '#e5c07b' },
+    --     --     -- ["@string"] = {
+    --     --     --   fg = '$bright_orange',
+    --     --     --   bg = '#00ff00',
+    --     --     --   fmt = 'bold'
+    --     --     -- },
+    --     --     ["@function"] = { fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic' },
+    --     --     ["@function.builtin"] = { fg = '#0059ff' }
+    --     ["@keyword"] = { fmt = 'italic' },
+    --   }
+    -- }
+    --
+    vim.cmd.colorscheme 'onedark'
+  end,
+}
