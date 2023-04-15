1cc77d70c62aa011a0a4b35fb2af3c39d158da32
sabovyan <abovyan.sargis90@gmail.com>
2023-04-15 16:21:31 +0400

    customize blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -6,5 +6,8 @@ return {
   opts = {
     char = '┊',
     show_trailing_blankline_indent = false,
+    show_end_of_line = true,
+    space_char_blankline = " ",
+    show_current_context = true,
   },
 }
