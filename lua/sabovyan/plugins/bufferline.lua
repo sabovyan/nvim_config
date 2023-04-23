862890eb48d093bce0825033c6f7faf95a2fe967
sabovyan <abovyan.sargis90@gmail.com>
2023-04-23 10:11:23 +0400

    fix bufferline and neo tree alignment

--- lua/sabovyan/plugins/bufferline.lua
+++ lua/sabovyan/plugins/bufferline.lua
@@ -4,6 +4,19 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require("bufferline").setup {
+      options = {
+        diagnostics = "nvim_lsp",
+        show_buffer_icon = true,
+        show_buffer_close_icon = true,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            highlight = "Directory",
+            separator = true
+          }
+        }
+      }
     }
   end
 }
