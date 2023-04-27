ce2f49cc11d59c0c302373efbfc9bee5c7c9efce
sabovyan <abovyan.sargis90@gmail.com>
2023-04-27 09:58:04 +0400

    customize bufferline

--- lua/sabovyan/plugins/bufferline.lua
+++ lua/sabovyan/plugins/bufferline.lua
@@ -5,7 +5,38 @@ return {
   config = function()
     require("bufferline").setup {
       options = {
+        custom_areas = {
+          right = function()
+            local result = {}
+            local seve = vim.diagnostic.severity
+            local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+            local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+            local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+            local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+            if error ~= 0 then
+              table.insert(result, { text = "  " .. error, fg = "#EC5241" })
+            end
+
+            if warning ~= 0 then
+              table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
+            end
+
+            if hint ~= 0 then
+              table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
+            end
+
+            if info ~= 0 then
+              table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
+            end
+            return result
+          end,
+        },
         diagnostics = "nvim_lsp",
+        -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+        diagnostics_indicator = function(count --[[ level, diagnostics_dict, context ]])
+          return "(" .. count .. ")"
+        end,
         show_buffer_icon = true,
         show_buffer_close_icon = true,
         offsets = {
