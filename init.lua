d9457194ce091131116f06dc1d93131a3b4f117c
sabovyan <abovyan.sargis90@gmail.com>
2023-05-01 15:26:02 +0400

    remove uc

--- init.lua
+++ init.lua
@@ -3,14 +3,6 @@ local main = function()
     -- VSCode extension
   else
     require("sabovyan")
-    -- local currentDir = vim.fn.getcwd()
-    --
-    -- local lower = string.lower(currentDir)
-    -- local isUcraft = string.find(lower, 'uc')
-    --
-    -- if isUcraft then
-    require("uc")
-    -- end
   end
 end
 
