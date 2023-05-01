c3f5c3466d827659687eca23d1753a45bcdf91a7
sabovyan <abovyan.sargis90@gmail.com>
2023-05-01 17:49:01 +0400

    customize eslint configs

--- lua/sabovyan/plugins/null-ls.lua
+++ lua/sabovyan/plugins/null-ls.lua
@@ -13,7 +13,7 @@ return {
     end
 
     -- for conciseness
-    local formatting = null_ls.builtins.formatting   -- to setup formatters
+    local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- to setup format on save
@@ -26,14 +26,15 @@ return {
         --  to disable file types use
         --  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
         formatting.prettier, -- js/ts formatter
-        formatting.stylua,   -- lua formatter
-        diagnostics.eslint_d.with({
-          -- js/ts linter
-          -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-          condition = function(utils)
-            return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-          end,
-        }),
+        formatting.stylua, -- lua formatter
+        diagnostics.eslint,
+        -- .with({
+        -- js/ts linter
+        -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+        -- condition = function(utils)
+        --   return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+        -- end,
+        -- }),
       },
       -- configure format on save
       on_attach = function(current_client, bufnr)
