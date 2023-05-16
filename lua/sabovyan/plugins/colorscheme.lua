aaaceea25dbc6676e90d323f418cb6f689753d66
sabovyan <abovyan.sargis90@gmail.com>
2023-05-16 11:13:42 +0400

    add custom hilights

--- lua/sabovyan/plugins/colorscheme.lua
+++ lua/sabovyan/plugins/colorscheme.lua
@@ -61,7 +61,35 @@ return {
           crust = "#151819",
         },
       },
+      custom_highlights = function(colors)
+        return {
+          NormalFloat = { bg = colors.crust },
+          FloatBorder = { bg = colors.crust, fg = colors.crust },
+          VertSplit = { bg = colors.base, fg = colors.surface0 },
+          CursorLineNr = { fg = colors.mauve },
+          Pmenu = { bg = colors.crust, fg = "" },
+          PmenuSel = { bg = colors.surface0, fg = "" },
+          TelescopeSelection = { bg = colors.surface0 },
+          TelescopePromptCounter = { fg = colors.mauve },
+          TelescopePromptPrefix = { bg = colors.surface0 },
+          TelescopePromptNormal = { bg = colors.surface0 },
+          TelescopeResultsNormal = { bg = colors.mantle },
+          TelescopePreviewNormal = { bg = colors.crust },
+          TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+          TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+          TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+          TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
+          TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
+          TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
+          IndentBlanklineChar = { fg = colors.surface0 },
+          IndentBlanklineContextChar = { fg = colors.surface2 },
+          GitSignsChange = { fg = colors.peach },
+          NvimTreeIndentMarker = { link = "IndentBlanklineChar" },
+          NvimTreeExecFile = { fg = colors.text },
+        }
+      end,
     })
+
     -- setup must be called before loading
     vim.cmd.colorscheme("catppuccin")
   end,
