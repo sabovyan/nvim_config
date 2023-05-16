9523c0f9dcd0265aba17493ae3ba5a3830e7b573
sabovyan <abovyan.sargis90@gmail.com>
2023-05-16 11:14:40 +0400

    fix linting

--- lua/sabovyan/plugins/blankline.lua
+++ lua/sabovyan/plugins/blankline.lua
@@ -1,10 +1,10 @@
 return {
   -- Add indentation guides even on blank lines
-  'lukas-reineke/indent-blankline.nvim',
+  "lukas-reineke/indent-blankline.nvim",
   -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help indent_blankline.txt`
   opts = {
-    char = '┊',
+    char = "┊",
     show_trailing_blankline_indent = false,
     show_end_of_line = true,
     space_char_blankline = " ",
